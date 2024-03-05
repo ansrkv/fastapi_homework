afcf9cfe0ecf0b15b1388bb89fdf0d65aa1b6547
ansrkv <145799225+ansrkv@users.noreply.github.com>
2024-03-05 17:33:43 +0300

    Update __init__.py

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from routers.v1.sellers import sellers_router
-from routers.v1.books import books_router
-from routers.v1.sellers import sellers_router
+from src.routers.v1.sellers import sellers_router
+from src.routers.v1.books import books_router
+from src.routers.v1.sellers import sellers_router
 
 v1_router = APIRouter(tags=["v1"], prefix="/api/v1")
 
